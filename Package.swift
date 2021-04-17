d8525b2e9c98dec56b239c88e0367e66c252c4a2
kzhiquan <kzhiquan@gmail.com>
2021-04-17 18:57:57 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyGif",
-    type: .dynamic,
     platforms: [
         .iOS("9.0"), .macOS(.v10_12),
     ],
     products: [
         .library(
             name: "SwiftyGif",
+            type: .dynamic,
             targets: ["SwiftyGif"]),
     ],
     dependencies: [],
